bfb00b6949868e95e58ca6b2a47a579c149f2f79
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2025-07-29 19:04:10 -0300

    Update init.sh

--- debian-bind-docker/root/init.sh
+++ debian-bind-docker/root/init.sh
@@ -3,8 +3,10 @@
 # Fail on any error
 set -e
 
-# Start zone updater in background
+# Set executable permissions for the update script
 chmod +x /update_zone.sh
+
+# Start zone updater in background
 /update_zone.sh &
 
 # Start named in foreground
